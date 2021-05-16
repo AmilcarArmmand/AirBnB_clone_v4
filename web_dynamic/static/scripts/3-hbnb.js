721739c6a1741230c06f9000a57dd2c5fb88dfa6
Mfeliciano778 <1652@holbertonschool.com>
2021-05-16 21:09:48 +0000

    Adding population of places

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -20,27 +20,36 @@ $(document).ready(function () {
         $('div#api_status').removeClass('available');
       }
     });
-    // $.post("http://127.0.0.1:5001/api/v1/places_search", {}, function (data, textStatus) {
-		// 	if (data.status === 'OK') {
-		// 		console.log("HEY!");
-		// 	}
-    // });
 		$.ajax({
-			url: 'http://localhost:5001/api/v1/places_search',
-			METHOD: 'POST',
-			contentType: 'application/json',
+			type: 'POST',
+      contentType: 'application/json',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
 			data: JSON.stringify({}),
 			dataType: 'json',
 			success: function( data, textStatus, jQxhr ){
-				console.log("HEY!");
+        const divTitle = "<article><div class='title_box'><h2></h2><div class='price_by_night'></div></div>";
+        const divInfo = "<div class='information'><div class='max_guest'></div><div class='number_rooms'></div>";
+        const divBaths = "<div class='number_bathrooms'></div></div><div class='description'></div></article>";
+        const article = divTitle + divInfo + divBaths;
+        $( data ).each(function (index, value) {
+          $('section.places').append(article);
+          // console.log(index)
+          // console.log(value)
+          $('.title_box h2').last().html(value.name);
+          // console.log(value.price_by_night)
+          $('.price_by_night').last().html("$" + value.price_by_night);
+          // console.log(value.max_guest)
+          $('.max_guest').last().html(value.max_guest);
+          // console.log(value.number_rooms)
+          $('.number_rooms').last().html(value.number_rooms);
+          // console.log(value.number_bathrooms)
+          $('.number_bathrooms').last().html(value.number_bathrooms);
+          // console.log(value.description)
+          $('.description').last().html(value.description);
+        });
 			},
 			error: function( jqXhr, textStatus, errorThrown ){
 					console.log( errorThrown );
-			},
-			statusCode: {
-				405: function() {
-					alert( "still not working" );
-				}
 			}
 		});
   });
